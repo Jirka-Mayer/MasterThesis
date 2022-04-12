aa1e2dd823bfff4cd2bb292eadb67e0e734566a9
Jirka-Mayer <jirkamayer97@gmail.com>
2022-04-12 11:12:47 +0200

    Optimized deepscores metadata memory usage

--- code/app/datasets/deepscores/DsMetadata.py
+++ code/app/datasets/deepscores/DsMetadata.py
@@ -8,7 +8,17 @@ from .SegmentationColors import SegmentationColors
 class DsMetadata:
     def __init__(self, json_path: str):
         with open(json_path, "r") as f:
-            self.ds = json.load(f)
+            ds = json.load(f)
+
+        self._categories = ds["categories"]
+        self._image_sizes_wh = [
+            (img["width"], img["height"]) for img in ds["images"]
+        ]
+        self._image_filenames = [
+            img["filename"] for img in ds["images"]
+        ]
+
+        del ds # free up memory
 
     @staticmethod
     def from_test_set():
@@ -19,27 +29,27 @@ class DsMetadata:
         return DsMetadata(DEEPSCORES_TRAIN_ANNOTATIONS)
 
     def page_index_to_image_path(self, page_index: int):
-        filename = self.ds["images"][page_index]["filename"]
+        filename = self._image_filenames[page_index]
+
         return os.path.join(DEEPSCORES_PATH, "images", filename)
 
     def page_index_to_segmentation_path(self, page_index: int):
-        filename = self.ds["images"][page_index]["filename"]
+        filename = self._image_filenames[page_index]
+
         filename = filename[:-len(".png")]
         filename += "_seg.png"
         return os.path.join(DEEPSCORES_PATH, "segmentation", filename)
 
     def page_index_to_size_wh(self, page_index: int) -> Tuple[int, int]:
-        w = self.ds["images"][page_index]["width"]
-        h = self.ds["images"][page_index]["height"]
-        return w, h
+        return self._image_sizes_wh[page_index]
 
     def classes_to_colors_rgb(self, classes: List[str]) -> List[Tuple[int, int, int]]:
         color_indices = [
-            c["color"] for c in self.ds["categories"].values()
+            c["color"] for c in self._categories.values()
             if c["annotation_set"] == "deepscores" and c["name"] in classes
         ]
         colors_rgb = [SegmentationColors.RGB[i] for i in color_indices]
         return colors_rgb
 
     def page_count(self) -> int:
-        return len(self.ds["images"])
+        return len(self._image_filenames)

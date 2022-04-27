ad4fcc741b53183c0c059091a1941184a5037000
Jirka-Mayer <jirkamayer97@gmail.com>
2022-04-27 20:39:08 +0200

    cvc-improvements charts

--- figures/02-cvc-improvements/README.md
+++ figures/02-cvc-improvements/README.md
@@ -11,7 +11,7 @@ The commands to compute the values:
 ~/interactive.sh ~/pyenv/bin/python3 main.py unet train --dataset improvement --val_pages 20 --sup_pages 99 --unsup_pages 0 --epochs 50 --inner_features 16
 ~/interactive.sh ~/pyenv/bin/python3 main.py unet train --dataset improvement --val_pages 20 --sup_pages 99 --unsup_pages 0 --epochs 50 --inner_features 32
 
-# semu-supervised models
+# semi-supervised models
 ~/interactive.sh ~/pyenv/bin/python3 main.py unet train --dataset improvement --val_pages 20 --sup_pages 99 --unsup_pages 551 --epochs 50 --inner_features 1
 ~/interactive.sh ~/pyenv/bin/python3 main.py unet train --dataset improvement --val_pages 20 --sup_pages 99 --unsup_pages 551 --epochs 50 --inner_features 2
 ~/interactive.sh ~/pyenv/bin/python3 main.py unet train --dataset improvement --val_pages 20 --sup_pages 99 --unsup_pages 551 --epochs 50 --inner_features 4
--- figures/02-cvc-improvements/plot.py
+++ figures/02-cvc-improvements/plot.py
@@ -1,1 +1,58 @@
-print("TODO")
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import shutil
+
+
+if len(sys.argv) >= 2 and sys.argv[1] == "pull":
+    print("Pulling...")
+    SOURCE_DIR = "/run/user/1000/gvfs/sftp:host=aic.ufal.mff.cuni.cz,user=mayerji/home/mayerji/MastersThesis/code/experiments-data/unet"
+    for name in os.listdir("data"):
+        dirname = name[:-len(".csv")]
+        shutil.copyfile(
+            os.path.join(SOURCE_DIR, dirname, "metrics.csv"),
+            os.path.join("data", name)
+        )
+    print("Done.")
+
+
+def get_file(unsup=0, ft=1):
+    for name in os.listdir("data"):
+        if "unsup={}".format(unsup) in name:
+            if "ft={}".format(ft) in name:
+                return name
+    raise Exception("File not found")
+
+
+def plot_data(unsup=0):
+    for ft in [1, 2, 4, 8, 16, 32]:
+        data = np.genfromtxt(
+            "data/" + get_file(unsup=unsup, ft=ft),
+            delimiter=',', dtype=None, names=True
+        )
+        plt.plot(
+            data["epoch"] + 1,
+            data["val_f1_score"],
+            label="{}ft".format(ft)
+        )
+    plt.xlabel("Epoch")
+    plt.ylabel("F1 score")
+
+
+plot_data(unsup=0)
+plt.legend(loc="lower right")
+plt.ylim([0.8, 0.97])
+plt.grid(axis="y")
+plt.title("Fully-supervised")
+plt.savefig("supervised.pdf")
+plt.close()
+
+
+plot_data(unsup=551)
+plt.legend(loc="lower right")
+plt.ylim([0.8, 0.97])
+plt.grid(axis="y")
+plt.title("Semi-supervised")
+plt.savefig("semisupervised.pdf")
+plt.close()
